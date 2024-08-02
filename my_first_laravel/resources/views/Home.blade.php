e32d313f9c150f304f41f2ef68246ba67b70837d
Hunter7210 <heitorrmsilva@gmail.com>
2024-08-02 15:25:13 -0300

    Firsts steps in Laravel

--- my_first_laravel/resources/views/Home.blade.php
+++ my_first_laravel/resources/views/Home.blade.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Inicial</title>
-    <link rel="stylesheet" href="{{ asset('css/navbar.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/home.css') }}"> <!-- Adicione seu CSS específico para a página inicial -->
+    <link rel="stylesheet" href="{{asset('css/navbar.css')}}">
+    <link rel="stylesheet" href="{{asset('css/footer.css')}}">
+    <link rel="stylesheet" href="{{asset('css/home.css')}}">
 </head>
 
 <body>
